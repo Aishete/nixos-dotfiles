9b98a20fae22c7010fe2c910b429151ba6781087
Aishete <trybunhengy@gmail.com>
2026-05-26 09:15:32 +0700

    add component for rust

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -37,6 +37,9 @@
     # aider-chat # AI in terminal (Optional: Client only)
     cmatrix # Matrix Movie Effect In Terminal
     alsa-utils
+    libxi
+    libxrandr
+    libxkbcommon
     # cowsay # Great Fun Terminal Program
     # duf # Utility For Viewing Disk Usage In Terminal
     # dysk # Disk space util nice formattting
