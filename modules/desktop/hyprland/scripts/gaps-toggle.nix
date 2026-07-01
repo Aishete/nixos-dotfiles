c61c37e0ea255321d79b582921c375c77d933e1d
Aishete <trybunhengy@gmail.com>
2026-07-01 16:58:11 +0700

    fix(gaps-toggle): use JSON output and handle custom type format

--- modules/desktop/hyprland/scripts/gaps-toggle.nix
+++ modules/desktop/hyprland/scripts/gaps-toggle.nix
@@ -1,8 +1,15 @@
 { pkgs, ... }:
 pkgs.writeShellScriptBin "gaps-toggle" ''
-  CURRENT=$(hyprctl getoption general:gaps_in | ${pkgs.gawk}/bin/awk 'NR==1 {print $2}')
+  CURRENT_GAPS_IN=$(
+    hyprctl -j getoption general:gaps_in | ${pkgs.gawk}/bin/awk -F'"' '/"custom"/ {split($4, a, " "); print a[1]}'
+  )
 
-  case $CURRENT in
+  case $CURRENT_GAPS_IN in
+    0)
+      hyprctl keyword general:gaps_in 4
+      hyprctl keyword general:gaps_out 5
+      notify-send "Gaps" "normal (4/5)"
+      ;;
     1)
       hyprctl keyword general:gaps_in 4
       hyprctl keyword general:gaps_out 5
@@ -13,7 +20,12 @@ pkgs.writeShellScriptBin "gaps-toggle" ''
       hyprctl keyword general:gaps_out 12
       notify-send "Gaps" "spacious (8/12)"
       ;;
-    8|*)
+    8)
+      hyprctl keyword general:gaps_in 1
+      hyprctl keyword general:gaps_out 4
+      notify-send "Gaps" "compact (1/4)"
+      ;;
+    *)
       hyprctl keyword general:gaps_in 1
       hyprctl keyword general:gaps_out 4
       notify-send "Gaps" "compact (1/4)"
