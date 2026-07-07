7b77d113ebff817de2f5a8f159ccdf8e5a17b610
Aishete <trybunhengy@gmail.com>
2026-07-08 06:35:52 +0700

    fix: zed config via home-manager sharedModules

--- modules/programs/misc/zed/default.nix
+++ modules/programs/misc/zed/default.nix
@@ -1,7 +1,11 @@
-{ pkgs, ... }:
+{ ... }:
 {
-  xdg.configFile = {
-    "zed/settings.json".source = ./settings.json;
-    "zed/keymap.json".source = ./keymap.json;
-  };
+  home-manager.sharedModules = [
+    {
+      xdg.configFile = {
+        "zed/settings.json".source = ./settings.json;
+        "zed/keymap.json".source = ./keymap.json;
+      };
+    }
+  ];
 }
